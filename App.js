7f72a21548ded2a8bd1eb8a0f41d7dbeeee48ed0
shreyasadiyodi <adiyodishreyas@gmail.com>
2019-03-26 10:57:52 +0530

    basic navigation setup

--- App.js
+++ App.js
@@ -7,43 +7,36 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
+import { createStackNavigator, createDrawerNavigator, createAppContainer, DrawerActions } from 'react-navigation';
+import Icon from "react-native-vector-icons/Ionicons";
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import SideMenu from './src/components/SideMenu';
+import TweetListScreen from './src/components/TweetListScreen';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+const stackNav = createStackNavigator({
+  TweetListScreen : {
+    screen: TweetListScreen,
+    navigationOptions: ({navigation}) => ({
+      title: "Tweets",
+      headerRight:(<TouchableOpacity onPress={() => navigation.dispatch(DrawerActions.toggleDrawer()) }>
+                      <Icon style={{ paddingRight: 20 }} name="ios-menu" size={30} />
+                  </TouchableOpacity>
+      )
+    })
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
 });
+
+const drawerNav = createDrawerNavigator({
+    Main: {
+      screen: stackNav,
+    }
+  }, {
+    contentComponent: SideMenu,
+    drawerPosition: 'right',
+    drawerWidth: Dimensions.get('window').width - 80,  
+});
+
+const AppContainer = createAppContainer(drawerNav);
+
+export default AppContainer;
---
+++ src/components/SideMenu/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+
+
+export default class SideMenu extends Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text> SideMenu </Text>
+        </View>
+      );
+    }
+  }
